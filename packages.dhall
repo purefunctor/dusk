7033879a0f2179bf6cc2044b06e99fdef6a0d617
PureFunctor <purefunctor@gmail.com>
2022-04-29 22:06:48 +0800

    Update to v0.15.0; use spec instead of test-unit

--- packages.dhall
+++ packages.dhall
@@ -1,5 +1,11 @@
 let upstream =
-      https://github.com/purescript/package-sets/releases/download/psc-0.14.7-20220418/packages.dhall
-        sha256:2523a5659d0f3b198ffa2f800da147e0120578842e492a7148e4b44f357848b3
+      https://github.com/purescript/package-sets/releases/download/psc-0.15.0-20220428/packages.dhall
+        sha256:8734be21e7049edeb49cc599e968e965442dad70e3e3c65a5c2d1069ec781d02
 
 in  upstream
+  with debug =
+    { repo = "https://github.com/PureFunctor/purescript-debug.git"
+    , version = "c695edfd353eac13063808a3516d24e3620bee5d"
+    , dependencies = [ "prelude", "functions" ]
+    }
+  with dodo-printer.version = "v2.2.0"
--- test/Main.purs
+++ test/Main.purs
@@ -3,52 +3,51 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Aff (Aff)
+import Effect.Aff (launchAff_)
 import Data.Lens (review)
 import Data.Map as Map
 import Data.Maybe (Maybe(..))
 import Data.Tuple.Nested ((/\))
 import Dusk.Ast.Type (Type(..), _Function, substituteTypes)
-import Test.Unit (suite, test)
-import Test.Unit.Main (runTest)
-import Test.Unit.Assert as Assert
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = runTest do
-  suite "Dusk.Ast.Type" do
-    test "substituteType" substituteTypesUnit
-  where
-  substituteTypesUnit :: Aff Unit
-  substituteTypesUnit =
-    let
-      mkFunction argument result = review _Function
-        { ann0: unit
-        , ann1: unit
-        , ann2: unit
-        , argument
-        , result
-        }
-      mkVariable name = Variable
-        { ann: unit
-        , name
-        }
-      mkForall name type_ = Forall
-        { ann: unit
-        , name: "a0"
-        , kind_: Nothing
-        , type_: Forall
+main = launchAff_ $ runSpec [ consoleReporter ] do
+  describe "Dusk.Ast.Type" do
+    describe "substituteType" do
+      it "removes any form of collisions" do
+        let
+          mkFunction argument result = review _Function
+            { ann0: unit
+            , ann1: unit
+            , ann2: unit
+            , argument
+            , result
+            }
+          mkVariable name = Variable
             { ann: unit
             , name
-            , kind_: Nothing
-            , type_
             }
-        }
-      replacements = Map.fromFoldable
-        [ "b" /\ mkVariable "a"
-        , "a1" /\ mkVariable "a2"
-        , "a2" /\ mkVariable "a3"
-        ]
-      actual = substituteTypes replacements $ mkForall "a" $ mkFunction (mkVariable "a") (mkVariable "b")
-      expected = mkForall "a4" $ mkFunction (mkVariable "a4") (mkVariable "a")
-    in
-      Assert.assert "a should be a4" $ actual == expected
+          mkForall name type_ = Forall
+            { ann: unit
+            , name: "a0"
+            , kind_: Nothing
+            , type_: Forall
+                { ann: unit
+                , name
+                , kind_: Nothing
+                , type_
+                }
+            }
+          replacements = Map.fromFoldable
+            [ "b" /\ mkVariable "a"
+            , "a1" /\ mkVariable "a2"
+            , "a2" /\ mkVariable "a3"
+            ]
+          actual = substituteTypes replacements $ mkForall "a" $ mkFunction (mkVariable "a") (mkVariable "b")
+          expected = mkForall "a4" $ mkFunction (mkVariable "a4") (mkVariable "a")
+
+        (actual == expected) `shouldEqual` true
