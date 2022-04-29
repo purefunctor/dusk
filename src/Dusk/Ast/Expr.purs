162a7842dea37dc435dc87222868505bea7cc585
PureFunctor <purefunctor@gmail.com>
2022-04-29 20:52:21 +0800

    Manually derive Eq and Ord for Expr

--- src/Dusk/Ast/Expr.purs
+++ src/Dusk/Ast/Expr.purs
@@ -26,6 +26,41 @@ data Expr a
   | Apply a (Expr a) (Expr a)
   | Annotate a (Expr a) (Type a)
 
-derive instance Eq a => Eq (Expr a)
-derive instance Ord a => Ord (Expr a)
+instance Eq (Expr a) where
+  eq = case _, _ of
+    Literal _ a, Literal _ b ->
+      a == b
+    Variable _ a, Variable _ b ->
+      a == b
+    Lambda _ a b, Lambda _ c d ->
+      a == c && b == d
+    Apply _ a b, Apply _ c d ->
+      a == c && b == d
+    Annotate _ a b, Annotate _ c d ->
+      a == c && b == d
+    _, _ ->
+      false
+
+instance Ord (Expr a) where
+  compare = case _, _ of
+    Literal _ a, Literal _ b ->
+      a `compare` b
+    Variable _ a, Variable _ b ->
+      a `compare` b
+    Lambda _ a b, Lambda _ c d ->
+      a `compare` c <> b `compare` d
+    Apply _ a b, Apply _ c d ->
+      a `compare` c <> b `compare` d
+    Annotate _ a b, Annotate _ c d ->
+      a `compare` c <> b `compare` d
+    a, b ->
+      exprIndex a `compare` exprIndex b
+    where
+    exprIndex = case _ of
+      Literal _ _ -> 0
+      Variable _ _ -> 1
+      Lambda _ _ _ -> 2
+      Apply _ _ _ -> 3
+      Annotate _ _ _ -> 4
+
 derive instance Functor Expr
