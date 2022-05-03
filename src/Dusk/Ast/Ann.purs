ee1b5596ac958d2087fb35ba50a5d995442a845c
PureFunctor <purefunctor@gmail.com>
2022-05-03 11:09:40 +0800

    Define base annotation types

---
+++ src/Dusk/Ast/Ann.purs
@@ -0,0 +1,32 @@
+module Dusk.Ast.Ann where
+
+import Prelude
+
+newtype SourcePos = SourcePos
+  { line :: Int
+  , column :: Int
+  }
+
+derive newtype instance Eq SourcePos
+derive newtype instance Ord SourcePos
+
+newtype SourceSpan = SourceSpan
+  { start :: SourcePos
+  , end :: SourcePos
+  }
+
+derive newtype instance Eq SourceSpan
+derive newtype instance Ord SourceSpan
+
+-- | The origin of a syntax node
+-- |
+-- | * abyss - defined in the compiler
+-- | * source - defined in a source file
+-- | * derived - defined from another node
+data From
+  = FromAbyss
+  | FromSource { span :: SourceSpan }
+  | FromDerived { from :: From }
+
+derive instance Eq From
+derive instance Ord From
